f1baef522171e5266e0d7d08490cde06914f96b9
eshaema <132393790+eshaema@users.noreply.github.com>
2024-05-25 22:13:57 +0500

    Esha

--- Schema/SQLQuery1.sql
+++ Schema/SQLQuery1.sql
@@ -7,3 +7,27 @@ username varchar(max) NULL,
 password varchar(max) NULL,
 date_create Date NULL
 );
+select * from users
+CREATE TABLE income(
+id Int PRIMARY KEY IDENTITY(1,1),
+category varchar(max) NULL,
+item varchar(max) NULL,
+income float NULL,
+description text null,
+date_income date null,
+date_insert date null
+);
+select * from income
+
+CREATE TABLE expenses(
+id INT PRIMARY KEY IDENTITY(1,1),
+category VARCHAR(MAX) NULL,
+item VARCHAR(MAX) NULL,
+cost FLOAT NULL,
+description VARCHAR(MAX) NULL,
+date_expense DATE NULL,
+date_insert DATE NULL
+);
+
+SELECT * FROM expenses
+
--- Schema/SQLQuery2.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE income(
-id Int PRIMARY KEY IDENTITY(1,1),
-category varchar(max) NULL,
-item varchar(max) NULL,
-income float NULL,
-description text null,
-date_income date null,
-date_insert date null
-);
